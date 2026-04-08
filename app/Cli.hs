b22a3effbc77bb6c85b9aabe3ceb1d694fc63a20
Dimitrios Papakonstantinou <papakonstantinou.dm@gmail.com>
2026-04-08 22:33:38 +0300

    Add Cli module

---
+++ app/Cli.hs
@@ -0,0 +1,33 @@
+-- | Command line interface definition
+module Cli where
+
+import Options.Applicative
+
+-- | CLI Options
+data Options = Options
+  { sourceFile :: [FilePath] -- ^ Source file
+  , optimizations :: Bool     -- ^ Enable optimizations
+  , outputFile :: Maybe FilePath    -- ^ Output file emited by compiler      
+  } deriving (Show)
+
+-- | Parser for CLI options
+optionsParser :: Parser Options
+optionsParser = Options
+  <$> some (argument str
+        ( metavar "SOURCE_FILES..."
+       <> help "Source file to process" ))
+  <*> switch
+        ( long "opt"
+       <> short 'O'
+       <> help "Enable compiler optimizations" )
+  <*> optional (option auto
+        ( long "output"
+       <> short 'o'
+       <> help "Output file emited by Compiler[default out.js]" ))
+
+-- | Full parser with help text
+optsInfo :: ParserInfo Options
+optsInfo = info (optionsParser <**> helper)
+  ( fullDesc
+ <> progDesc "Compile TypeJs files to readable javascript"
+ <> header "TypeJs - type-safe Javascript dialect" )
--- app/Main.hs
+++ app/Main.hs
@@ -8,16 +8,24 @@ import Typecheck
 import Desuger (lowerProgram)
 import Emit (emitProgram)
 import qualified Data.Text.IO as T
+import Options.Applicative
+import Cli
 
+-- TODO: Refactor this mess
 main :: IO ()
 main = do
-  src <- T.readFile "example.tjs"
-  case parseProgram "example.tjs" src of
+  opts <- execParser optsInfo
+  let file = head $ sourceFile opts
+      outputF = case (outputFile opts) of
+        Just ofile -> ofile
+        Nothing -> "out.js"
+  src <- T.readFile file
+  case parseProgram file src of
     Left perr -> putStrLn perr
     Right prog ->
         case inferProgram prog of
             Left terr -> putStrLn ("Type error: " <> show terr)
             Right _ -> do
                 let js = emitProgram (lowerProgram prog)
-                T.writeFile "out.js" js
+                T.writeFile outputF js
                 putStrLn "Generated out.js"
